d834390ee819bb1f61c022a8a50f009057f4468b
DESKTOP-210N03T\Valdeir <valdeircn11@gmail.com>
2021-06-02 21:53:13 -0300

    Criação do menu

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
+import { TecnicoReadComponent } from './pages/components/tecnico/tecnico-read/tecnico-read.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'tecnicos',
+    component: TecnicoReadComponent,
+  },
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
